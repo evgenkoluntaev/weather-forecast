662f70322a0fc8b34dbd457556763ac1d9273d77
evgenkoluntaev <evgen.koluntaev@gmail.com>
2021-10-13 17:02:04 +0300

    input check added

--- Weather.py
+++ Weather.py
@@ -2,16 +2,22 @@ from pyowm import OWM
 
 owm = OWM('a464f9ec8c423ca4231d871fa1820406')
 mgr = owm.weather_manager()
+while True:
+    try:
+        place = input('Select your city: ')
 
-place = input('Select your city: ')
+        observation = mgr.weather_at_place(place)
+        w = observation.weather
 
-observation = mgr.weather_at_place(place)
-w = observation.weather
+        t = w.temperature('celsius')['temp']
 
-t = w.temperature('celsius')['temp']
+        humi = w.humidity
 
-humi = w.humidity
+        time = w.reference_time('iso')
 
-time = w.reference_time('iso')
+        print(place+': '+w.detailed_status+' now.\nTemperature is '+str(t)+' celsius.\nHumidity: '+str(humi)+'%\nUpdated:'+time)
+        break
 
-print(place+': '+w.detailed_status+' now.\nTemperature is '+str(t)+' celsius.\nHumidity: '+str(humi)+'%\nUpdated:'+time)
+    except:
+        print('Please, enter the correct value.')
+
