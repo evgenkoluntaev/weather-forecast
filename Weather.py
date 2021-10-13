efa929e67facedbd82812fedf2e0780ef3875fed
evgenkoluntaev <evgen.koluntaev@gmail.com>
2021-10-13 16:49:27 +0300

    first commit

---
+++ Weather.py
@@ -0,0 +1,17 @@
+from pyowm import OWM
+
+owm = OWM('a464f9ec8c423ca4231d871fa1820406')
+mgr = owm.weather_manager()
+
+place = input('Select your city: ')
+
+observation = mgr.weather_at_place(place)
+w = observation.weather
+
+t = w.temperature('celsius')['temp']
+
+humi = w.humidity
+
+time = w.reference_time('iso')
+
+print(place+': '+w.detailed_status+' now.\nTemperature is '+str(t)+' celsius.\nHumidity: '+str(humi)+'%\nUpdated:'+time)
